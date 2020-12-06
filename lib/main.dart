dca7c117fbda470e9cbeb9a14c6d846334670f9e
Sanket Shah <shahsanket2107@gmail.com>
2020-12-06 15:52:57 +0530

    Const,Final and if else added

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      "questionText": "What is your nationality?",
+      "answers": ["Indian", "Pakistani", "Chinese", "American"],
+    },
+    {
+      "questionText": "What is your favourite color?",
+      "answers": ["Blue", "Red", "Green", "Yellow"],
+    },
+    {
+      "questionText": "What is your gender?",
+      "answers": ["Male", "Female", "Other"],
+    }
+  ];
+
   var questionIndex = 0;
 
 // _ (underscore) before a variable name or a class name denotes that it is private
@@ -19,40 +34,36 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       questionIndex += 1;
     });
+    if (questionIndex < questions.length) {
+      print("More questions are left");
+    } else {
+      print("No more questions left");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        "questionText": "What is your nationality?",
-        "answers": ["Indian", "Pakistani", "Chinese", "American"],
-      },
-      {
-        "questionText": "What is your favourite color?",
-        "answers": ["Blue", "Red", "Green", "Yellow"],
-      },
-      {
-        "questionText": "What is your gender?",
-        "answers": ["Male", "Female", "Other"],
-      }
-    ];
     return MaterialApp(
       title: 'First App',
       home: Scaffold(
           appBar: AppBar(
             title: Text('First App'),
           ),
-          body: Column(
-            children: [
-              Question(questions[questionIndex]['questionText']),
-              ...(questions[questionIndex]['answers'] as List<String>)
-                  .map((answer) {
-                print(answer);
-                return Answer(answerQuestion, answer);
-              })
-            ],
-          )),
+          body: questionIndex < questions.length
+              ? Column(
+                  children: [
+                    Question(questions[questionIndex]['questionText']),
+                    ...(questions[questionIndex]['answers'] as List<String>)
+                        .map((answer) {
+                      print(answer);
+                      return Answer(answerQuestion, answer);
+                    })
+                  ],
+                )
+              : Center(
+                  child:
+                      Text("Quiz is over!!!", style: TextStyle(fontSize: 28)),
+                )),
     );
   }
 }
